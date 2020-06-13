328fd589304341624286c73472e67ef81a042273
heymanpreet <manpreet.singh.chawla94@gmail.com>
2020-06-13 14:22:55 +0530

    moving genre routes to genres.js
    Removing /api/genre as we have told express to move the routes starting with this in genre.js

--- Vidly/index.js
+++ Vidly/index.js
@@ -1,71 +1,10 @@
 const express = require('express');
 const app = express();
+const genres = require('./routes/genres');
 const Joi = require('@hapi/joi');
 //To work with request body
 app.use(express.json());
-
-// genres = [{id:1,genre:'Action'}, {id:2,genre:'Adventure'}, {id:3,genre:'Comedy'}, {id:4,genre:'Horror'}];
-genres = [];
-
-// GET Genres
-app.get('/', (req,res) => {
-    res.send("Welcome to Vidly, Your Personal Movie Platform.");
-})
-
-// GET Genres
-app.get('/api/genres', (req,res) => {
-    res.send(genres);
-})
-
-// POST Genres
-app.post('/api/genres', (req,res) => {
-    // check for bad request
-    const {error} = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-    // console.log(req.body);
-    
-    // Valid genre
-    const genre = {
-        id: genres.length + 1,
-        genre: req.body.genre
-    }
-    genres.push(genre);
-    res.send(genre);
-})
-
-// Put Genre
-app.put('/api/genres/:id', (req,res) => {
-    // check for invalid input
-    const genreName = genres.find(g => g.id === parseInt(req.params.id));
-    // 404 Error check
-    if(!genreName) return res.status(404).send("Genre does not exist");
-    // 400 Error check
-    const {error} = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-    // Valid genre
-    genreName.genre = req.body.genre;
-    res.send(genreName)
-})
-
-// Delete Genre
-app.delete('api/genres/:id', (req,res) => {
-    // check for invalid input
-    const genreName = genres.find(g => g.id === parseInt(req.params.genre));
-    if(!genreName) return res.status(404).send("Genre does not exist");
-    //Valid Genre
-    const index = genres.indexOf(genreName);
-    genres.splice(index,1);
-    return res.send(`genre ${req.params.id} successfully deleted`);
-})
-
-// Validate Function
-function validateGenre(genre) {
-    const schema = Joi.object({
-        genre: Joi.string().required()
-    })
-    return schema.validate(genre);
-}
-
+app.use('/api/genres',genres);
 
 
 const port = process.env.PORT || 3000;
---
+++ Vidly/routes/genres.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+
+
+genres = [{id:1,genre:'Action'}, {id:2,genre:'Adventure'}, {id:3,genre:'Comedy'}, {id:4,genre:'Horror'}];
+
+// GET Genres
+router.get('/', (req,res) => {
+    res.send("Welcome to Vidly, Your Personal Movie Platform.");
+})
+
+// GET Genres
+router.get('/', (req,res) => {
+    res.send(genres);
+})
+
+// POST Genres
+router.post('/', (req,res) => {
+    // check for bad request
+    const {error} = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    // console.log(req.body);
+    
+    // Valid genre
+    const genre = {
+        id: genres.length + 1,
+        genre: req.body.genre
+    }
+    genres.push(genre);
+    res.send(genre);
+})
+
+// Put Genre
+router.put('/:id', (req,res) => {
+    // check for invalid input
+    const genreName = genres.find(g => g.id === parseInt(req.params.id));
+    // 404 Error check
+    if(!genreName) return res.status(404).send("Genre does not exist");
+    // 400 Error check
+    const {error} = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    // Valid genre
+    genreName.genre = req.body.genre;
+    res.send(genreName)
+})
+
+// Delete Genre
+router.delete('/:id', (req,res) => {
+    // check for invalid input
+    const genreName = genres.find(g => g.id === parseInt(req.params.genre));
+    if(!genreName) return res.status(404).send("Genre does not exist");
+    //Valid Genre
+    const index = genres.indexOf(genreName);
+    genres.splice(index,1);
+    return res.send(`genre ${req.params.id} successfully deleted`);
+})
+
+// Validate Function
+function validateGenre(genre) {
+    const schema = Joi.object({
+        genre: Joi.string().required()
+    })
+    return schema.validate(genre);
+}
+
+module.exports = router;
