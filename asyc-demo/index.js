b00df86ed5d4ec8ba514d033a2466783a2efd282
heymanpreet <manpreet.singh.chawla94@gmail.com>
2020-06-14 10:16:51 +0530

    callback hell solution

--- asyc-demo/index.js
+++ asyc-demo/index.js
@@ -2,21 +2,34 @@ console.log("before");
 //const userData = getUser(1); // we cant get data from async function like this.
 //console.log(userData);// we will get undefined because it will be executed first & results will be available later
 //when the result is ready user function will be called
-getUser(1,function (user) {
-    console.log(user);
-    // Get Repos
-    getRepositories(user.gihubUserName,(repos) => {
-        console.log(repos);
-    })  
-})
-console.log("after");
+// Callback hell
+// getUser(1,(user) => {
+//     console.log(user);
+//     getRepositories(user.gihubUserName, repos => {
+//         console.log(`repositories: ${repos}`);
+//         getCommits(repos,commits => {
+//             console.log(`commits: ${commits}`);
+//         })
+//     })
+// })
 
-// function getUser(id){
-//     setTimeout(() => {
-//         console.log("Reading Data from Database");
-//         return {"userId":id, gihubUserName:'mosh'}
-//     },2000);
-// }
+// Fixing issue of callback hell by defining naming function
+getUser(1,getRepoFromUser)
+console.log("after");
+function displayCommits(commits) {
+    console.log(`commits: ${commits}`);
+}
+
+function getCommitFromRepo(repos) {
+    console.log(`repositories: ${repos}`);
+    getCommits(repos,displayCommits);
+}
+
+function getRepoFromUser(user) {
+    console.log(user);
+    getRepositories(user.gihubUserName, getCommitFromRepo);
+}
+
 function getUser(id,callback){
     setTimeout(() => {
         console.log("Reading Data from Database");
@@ -30,3 +43,10 @@ function getRepositories(gitUser,callbackRepo) {
         callbackRepo(['Repo1','Repo2','Repo3']);
     },2000)
 }
+
+function getCommits(gitRepo,callback) {
+    setTimeout(() => {
+        console.log("getting commits of Repo");
+        callback(['Commit1','commit2','commit3']);
+    },2000)
+};
